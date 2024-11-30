f1cf40afca2181d2d171a9961934454fbdc80d76
Egemen Ozdag <31034722+egemenozdag@users.noreply.github.com>
2024-11-30 11:14:38 +0100

    Update test_clustering.py

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,1 +1,9 @@
+import pytest
+from clustering import train_kmeans
 
+def test_train_kmeans_valid_data():
+    data = [[1, 2], [2, 3], [3, 4]]
+    n_clusters = 2
+    model = train_kmeans(data, n_clusters)
+    assert model.n_clusters == n_clusters
+    assert len(model.cluster_centers_) == n_clusters
